e73d347ebb6beacd9ac5a6845c1cff0ddbb735c2
Yan Qian <qianyan@thoughtworks.com>
2016-01-08 20:22:28 +0800

    [lambeta] bowling scoring in clojure

---
+++ src/bowling/core.clj
@@ -0,0 +1,31 @@
+(ns bowling.core)
+
+(def STRIKE 10)
+
+(defn bowling-scoring [sequence]
+  (->> sequence
+       (re-seq #"\d+")
+       (map read-string)
+       (reduce (fn [xs x]
+                 (if (and (= STRIKE x) (even? (count xs)))
+                   (conj xs x 0)
+                   (conj xs x))) [])
+       (partition 2)
+       (map (fn [[f s]]
+              (cond
+                (= STRIKE f) [10 \x \y]
+                (= STRIKE (+ f s)) [f s \x]
+                :else [f s 0])))
+       (reduce (fn [xs [f s ss]]
+                 (conj (into [] (map (fn [x] (cond
+                                              (= \x x) f
+                                              (and (= \y x) (not (= \x s))) s
+                                              (= \y x) \x
+                                              :else x)) xs)) f s ss)))
+       (partition 3)
+       (take 10) ;; 10 => total rounds
+       (flatten)
+       (filter #(and (not= \x %) (not= \y %)))
+       (apply +)))
+
+
---
+++ test/bowling/core_test.clj
@@ -0,0 +1,24 @@
+(ns bowling.core-test
+  (:use midje.sweet)
+  (:use [bowling.core]))
+
+
+;.;. The right word may be effective, but no word was ever as effective as
+;.;. a rightly timed pause. -- Twain
+(facts "about `bowling-scoring`"
+  (fact "should add up all numbers if it is just basic score keeping"
+    (bowling-scoring "1 2 3 4") => 10)
+
+  (fact "should add next ball roll when there has the spare"
+    (bowling-scoring "9 1 9 1") => 29)
+  
+  (fact "should add the next two ball rolls when there has the strike"
+    (bowling-scoring "1 1 1 1 10 1 1") => 18)
+  
+  (fact "should score 300 pins when all 10 frames are strike"
+    (bowling-scoring "10 10 10 10 10 10 10 10 10 10 10 10") => 300)
+  
+  (fact "should score 279 pins when the first nine frames are strike but the tenth is spare"
+    (bowling-scoring "10 10 10 10 10 10 10 10 10 9 1 10") => 279)  )
+
+
